3e64d347d7f3a761e9eb2e573aeeaa90be462fbd
HannesOberreiter <hoberreiter@gmail.com>
2020-08-12 22:07:25 +0200

    added full url for twitter cards

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,16 +30,16 @@ export default {
       { hid: 'og:site_name', property: 'og:site_name', content: 'b.tree Beekeeping' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: URL },
-      { hid: 'og:image', property: 'og:image', content: `./card.png` },
+      { hid: 'og:image', property: 'og:image', content: `${URL}/card.png` },
       // Twitter Card
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
       { hid: 'twitter:site', name: 'twitter:site', content: '@btree_hannes' },
       { hid: 'twitter:title', name: 'twitter:title', content: 'b.tree' },
-      { hid: 'twitter:image', name: 'twitter:image', content: `./card.png` },
+      { hid: 'twitter:image', name: 'twitter:image', content: `${URL}/card.png` },
       { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: 'b.tree Beekeeping' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: `./favicon.ico` }
+      { rel: 'icon', type: 'image/x-icon', href: `${URL}/favicon.ico` }
     ]
   },
   css: [
