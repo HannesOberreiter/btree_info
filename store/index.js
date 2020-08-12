2c214581c295123f8ccd9533e5c4a431ebf186ea
HannesOberreiter <hoberreiter@gmail.com>
2020-08-12 19:02:27 +0200

    removed unsused release functions

--- store/index.js
+++ store/index.js
@@ -2,17 +2,13 @@ import Vue from 'vue'
 import groupBy from 'lodash.groupby'
 
 export const state = () => ({
-  categories: {},
-  releases: []
+  categories: {}
 })
 
 export const mutations = {
   SET_CATEGORIES (state, categories) {
     // Vue Reactivity rules since we add a nested object
     Vue.set(state.categories, this.$i18n.locale, categories)
-  },
-  SET_RELEASES (state, releases) {
-    state.releases = releases
   }
 }
 
@@ -27,35 +23,5 @@ export const actions = {
     const categories = groupBy(docs, 'category')
 
     commit('SET_CATEGORIES', categories)
-  },
-  /*async fetchReleases ({ commit }) {
-    const options = {}
-    if (process.env.GITHUB_TOKEN) {
-      options.headers = { Authorization: `token ${process.env.GITHUB_TOKEN}` }
-    }
-    let releases
-    try {
-      const data = await fetch('https://api.github.com/repos/nuxt/content/releases', options).then(res => res.json())
-      releases = data.filter(r => !r.draft).map((release) => {
-        return {
-          name: release.name || release.tag_name,
-          date: release.published_at,
-          body: this.$markdown(release.body)
-        }
-      })
-    } catch (e) {
-    }
-
-    const getMajorVersion = r => r.name && Number(r.name.substring(1, 2))
-    releases.sort((a, b) => {
-      const aMajorVersion = getMajorVersion(a)
-      const bMajorVersion = getMajorVersion(b)
-      if (aMajorVersion !== bMajorVersion) {
-        return bMajorVersion - aMajorVersion
-      }
-      return new Date(b.date) - new Date(a.date)
-    })
-
-    commit('SET_RELEASES', releases)
-  }*/
+  }
 }
