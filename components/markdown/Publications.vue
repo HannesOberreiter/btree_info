74a7ba8383f15e56ac7d0f97da243226a2201c0f
HannesOberreiter <hoberreiter@gmail.com>
2021-07-14 09:47:56 +0200

    added new widget for publications to remove reduntant code

---
+++ components/markdown/Publications.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+    <ol>
+        <li> Stefanec, M.; <b>Oberreiter</b>, H.; Becher, M. A.; Haase, G.; & Schmickl, T. Effects of Sinusoidal Vibrations on the Motion Response of Honeybees (2021). Frontiers in Physics, 9, 318. </li>
+        <li> <b>Oberreiter</b>, H.; Dünser, A.; Schlagbauer, J.; Brodschneider R. Das Wildnisgebiet Dürrenstein – ein Lebensraum für wildlebende Bienenvölker? Entomologica Austriaca (2021) – 0028: 25 - 42. </li>
+        <li> Zangl, L.; <b>Oberreiter</b>, H.; Huss, H. et al. Discriminating larvae of two syntopic <i>Cychramus</i> species (Coleoptera, Nitidulidae) by means of bar-HRM analysis (2020). Mol Biol Rep. </li>
+        <li> <b>Oberreiter</b>, H.; Brodschneider, R. Austrian COLOSS Survey of Honey Bee Colony Winter Losses 2018/19 and Analysis of Hive Management Practices. Diversity (2020), 12, 99. </li>
+    </ol>
+    <div class="flex justify-end">
+        <small>
+        <img alt="Orcid" src="/img/orcid.png" class="inline" style="margin-top: 0 rem; margin-bottom: 0 rem;" />
+        <a href=https://orcid.org/0000-0003-1019-1245>
+            ORCID
+        </a>
+        & 
+        <a href="https://scholar.google.com/citations?user=bs_92jEAAAAJ&hl">
+            Google Scholar
+        </a>
+        </small>
+    </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
