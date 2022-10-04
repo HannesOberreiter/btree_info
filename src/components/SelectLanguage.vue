49a1e53ba0f7ae51319823a7da9213220dadf8c9
HannesOberreiter <hoberreiter@gmail.com>
2022-10-04 18:14:29 +0200

    fix: :art: fix header on small devices

--- src/components/SelectLanguage.vue
+++ src/components/SelectLanguage.vue
@@ -3,6 +3,7 @@
     <select
       v-model="language"
       className="language-select"
+      class="language-select"
       :options="KNOWN_LANGUAGES"
       @change="changeLanguage(language)"
     >
@@ -40,12 +41,6 @@ function changeLanguage(lang: string) {
   width: 100%;
   flex-grow: 1;
   line-height: inherit;
-  /* Add dropdown arrow */
-  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%236b7280' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e");
-  background-position: 97%;
-  background-repeat: no-repeat;
-  background-size: 1.5em 1.5em;
-  -webkit-appearance: none;
 }
 
 @media (hover: hover) {
