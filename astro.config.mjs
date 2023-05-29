ad698f0bad982d6749e016b917fce61ecbd0abb8
HannesOberreiter <hoberreiter@gmail.com>
2023-05-29 14:41:39 +0200

    chore: :arrow_up:

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,13 +10,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // Important!
-  // Only official '@astrojs/*' integrations are currently supported by Astro.
-  // Add 'experimental.integrations: true' to make 'astro-robots-txt' working
-  // with 'astro build' command.
-  experimental: {
-    integrations: true,
-  },
   integrations: [
     vue(),
     tailwind({
