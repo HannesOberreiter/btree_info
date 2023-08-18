2a73b4b2604b866dda590a78e096a160014be3dd
HannesOberreiter <hoberreiter@gmail.com>
2023-08-18 12:49:47 +0200

    feat: :sparkles: add transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    viewTransitions: true,
+  },
   integrations: [
     vue(),
     tailwind({
