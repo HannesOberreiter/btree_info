e7669ae9fb4ca032553561eb45b6d37977c9f286
HannesOberreiter <hoberreiter@gmail.com>
2023-09-16 09:59:33 +0200

    fix: :bug: encapsulate vue component into one root element

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,19 +1,15 @@
 <template>
-  <h2 class="heading">Index</h2>
-  <ul>
-    <li
-      v-for="(item, index) in headings.filter(
+  <div>
+    <h2 class="heading">Index</h2>
+    <ul>
+      <li v-for="(item, index) in headings.filter(
         (o) => o.depth > 1 && o.depth < 4
-      )"
-      :class="`depth-${item.depth} ${
-        isActive(item.slug) ? 'text-amber-500' : ''
-      }`"
-      class="heading-link"
-      :key="index"
-    >
-      <a :href="`#${item.slug}`">{{ item.text }}</a>
-    </li>
-  </ul>
+      )" :class="`depth-${item.depth} ${isActive(item.slug) ? 'text-amber-500' : ''
+  }`" class="heading-link" :key="index">
+        <a :href="`#${item.slug}`">{{ item.text }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
